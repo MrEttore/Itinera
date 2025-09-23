e50fecc40fde5cb46feab92e9e948c59afdfc4ba
Ettore Marangon <marangonettore@gmail.com>
2025-09-23 11:16:15 +0200

    refactor: remove unused index.js file

--- src/pages/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Homepage } from './Homepage/Homepage';
-export { default as Pricing } from './Pricing/Pricing';
-export { default as Product } from './Product/Product';
-export { default as PageNotFound } from './PageNotFound/PageNotFound';
-export { default as AppLayout } from './AppLayout/AppLayout';
-export { default as Login } from './Login/Login';
